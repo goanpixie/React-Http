123e0984ce0ac971c7758cd78010a01000298c67
Priyanka <priyanka3007@gmail.com>
2019-02-08 14:40:25 -0800

    select a post id

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,7 +7,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts:[]
+        posts: [],
+        selectedPostId:null
     }
 
     componentDidMount() {
@@ -25,9 +26,17 @@ class Blog extends Component {
             })
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={()=>this.postSelectedHandler(post.id)}/>
         });
         return (
             <div>
@@ -35,7 +44,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.seletedPostId}/>
                 </section>
                 <section>
                     <NewPost />
