12377cf150d8a4065211ce15feaa55ba7e8c7404
Priyanka <priyanka3007@gmail.com>
2019-02-09 17:51:32 -0800

    comment

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './FullPost.css';
 
+//Full Post
 class FullPost extends Component {
     state = {
         loadedPost:null
