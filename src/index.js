dfa6a0e91ce40143db160a9ec6ec82361208dc19
Priyanka <priyanka3007@gmail.com>
2019-02-12 13:55:19 -0800

    axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
+axios.interceptors.request.use(request => {
+	console.log(request);
+	//Edit request
+	return request;
+})
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
