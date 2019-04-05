9388a75f5861d96594f8728c35f73959f1b49154
Priyanka <priyanka3007@gmail.com>
2019-04-05 15:54:03 -0700

    router path

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 
 
 class Blog extends Component {
@@ -16,6 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
+                <Route path="/" exact render={()=><h1>Home</h1>}/>
                 <Posts/>
 
             </div>
