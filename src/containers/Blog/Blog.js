894f3225d8fa1c99c1a2e1c8f13baef776c6cf5c
Priyanka <priyanka3007@gmail.com>
2019-04-05 16:09:43 -0700

    routing for posts possible

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,8 +17,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={()=><h1>Home</h1>}/>
-                <Posts/>
+                {/* <Route path="/" exact render={()=><h1>Home</h1>}/> */}
+                <Route path='/' exact component={Posts}/>
+                <Posts />
 
             </div>
         );
